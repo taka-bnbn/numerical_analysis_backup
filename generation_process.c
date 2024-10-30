2c72b36d0fecb864ab8dd9ca846a3fd66d28422a
Taka-bnbn <mizutaka2005@gmail.com>
2024-10-31 00:31:17 +0900

    add a command

--- generation_process.c
+++ generation_process.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <ctype.h>
-int main(void)
-	[
-	int isalpha(int c);
-	]
+
+int main() {
+    char str[1000];
+    int count[26] = {0}; // 各アルファベットの出現回数を記録する配列
+
+    // ユーザー入力
+    printf("文字列を入力してください: ");
+    fgets(str, sizeof(str), stdin);
+
+    // 文字列内の各文字について処理
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isalpha(str[i])) { // アルファベットかどうかチェック
+            char ch = tolower(str[i]); // 小文字に変換
+            count[ch - 'a']++; // 該当する位置にカウントを加算
+        }
+    }
+
+    // 結果を表示
+    printf("アルファベットの使用頻度:\n");
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, count[i]);
+    }
+
+    return 0;
+}
+
+
+
+
+
+
