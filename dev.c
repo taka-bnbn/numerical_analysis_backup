2d7e5954970df5a6ae1522e1bba765bb603f6546
Taka-bnbn <mizutaka2005@gmail.com>
2024-11-06 10:18:30 +0900

    add

---
+++ dev.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+int main() {
+    char kazu[1000];  
+    int count[26] = {0}; // アルファベットの出現回数を記録する配列
+    FILE *fp, *f;
+    int totalmojisu = 0; // 読み込んだ文字の総数
+    int kaisu = 0;
+    
+    f = fopen("data.txt", "r");
+    if (f == NULL) {
+        printf("開けませんでした。\n");
+        return 1;
+    }
+
+ 
+    while ((sizeof(kazu)/sizeof(kazu[0]))<=kaisu) {
+        
+    for (int i = 0; kazu[i] != '\0' && totalmojisu < 10000; i++) {
+            
+	    if (isalpha(kazu[i])) { // アルファベットかどうかチェック
+                char ch = tolower(kazu[i]); // 小文字に変換
+                count[ch - 'a']++;
+                totalmojisu++;
+		kaisu++;
+            }
+        }
+    }
+    fclose(f);
+
+    
+    printf("使用頻度:\n");
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, count[i]);
+    }
+
+    fp = fopen("count.txt", "w");
+    if (fp == NULL) {
+        printf("開けませんでした。\n");
+        return 1;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        fprintf(fp, "%c: %d\n", 'a' + i, count[i]);
+    }
+
+    fclose(fp);
+    return 0;
+}
+
