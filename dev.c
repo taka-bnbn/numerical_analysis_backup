57345fd8f34426f1f5d5bf080210492196339435
Taka-bnbn <mizutaka2005@gmail.com>
2024-11-06 11:03:37 +0900

    動かないブタはただのdevだ

--- dev.c
+++ dev.c
@@ -16,7 +16,7 @@ int main() {
     }
 
 	//少し複雑だなーって思ってます 
-    while ((sizeof(kazu)/sizeof(kazu[0]))<=kaisu) {//此処がよろしくないようですねぇ	
+    while ((sizeof(kazu)/sizeof(kazu[0]))>=kaisu) {//此処がよろしくないようですねぇ	
         
     for (int i = 0; kazu[i] != '\0' && totalmojisu < 10000; i++) {
             
