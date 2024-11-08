7773fb4c126b5db980ab916d042918379483bc5d
Taka-bnbn <mizutaka2005@gmail.com>
2024-11-08 15:41:02 +0900

    add a kadai2.c

--- kadai2.c
+++ kadai2.c
@@ -2,8 +2,8 @@
 #include <ctype.h>
 
 typedef struct {
-    char letter;
-    int frequency;
+    char hitomoji;
+    int hindo;
 } LetterFrequency;
 
 int main() {
@@ -22,35 +22,31 @@ int main() {
                 char ch = tolower(kazu[i]);
                 count[ch - 'a']++;
                 total_chars++;
-            }
-        }
-    }
+            }}}
+
     fclose(f);
 
     // アルファベットと頻度のペアを作成
     LetterFrequency frequencies[26];
     for (int i = 0; i < 26; i++) {
-        frequencies[i].letter = 'a' + i;
-        frequencies[i].frequency = count[i];
-    }
+        frequencies[i].hitomoji = 'a' + i;
+        frequencies[i].hindo = count[i];}
 
     // 並び替え
     for (int i = 0; i < 26 - 1; i++) {
         for (int j = i + 1; j < 26; j++) {
-            if (frequencies[i].frequency < frequencies[j].frequency) {
+            if (frequencies[i].hindo < frequencies[j].hindo) {
                 LetterFrequency temp = frequencies[i];
                 frequencies[i] = frequencies[j];
                 frequencies[j] = temp;
-            }
-        }
-    }
+            }}}
 
 
     fp = fopen("count.txt", "w");
 
     for (int i = 0; i < 26; i++) {
-        fprintf(fp, "%c: %d\n", frequencies[i].letter, frequencies[i].frequency);
-    }
+        fprintf(fp, "%c: %d\n", frequencies[i].hitomoji, frequencies[i].hindo);}
+
 
     fclose(fp);
     return 0;
